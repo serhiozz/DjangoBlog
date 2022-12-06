ce35f84f05b42b26cc5f56f6c439638318ba506d
Sergey <cergio78@gmail.com>
2022-12-06 20:23:29 +0700

    Доделана админка

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib import admin
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.utils.safestring import mark_safe
 
 from .models import *
 
@@ -8,13 +9,21 @@ from .models import *
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}  # Автозаполнение поля Slug из значения Title
 
+    list_display = ('id', 'title', 'slug')  # Отображаемые поля в списке
+    list_display_links = ('id', 'title')  # id и заголовок в виде ссылке в админке в списке
+    search_fields = ('title',)  # Возможность поиска по названию статьи
+
 
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
+    list_display = ('id', 'title', 'slug')  # Отображаемые поля в списке
+    list_display_links = ('id', 'title')  # id и заголовок в виде ссылке в админке в списке
+    search_fields = ('title',)  # Возможность поиска по названию статьи
+
 
 class PostAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorUploadingWidget())
+    content = forms.CharField(widget=CKEditorUploadingWidget(), label="Текст")
 
     class Meta:
         model = Post
@@ -23,7 +32,25 @@ class PostAdminForm(forms.ModelForm):
 
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
-    prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('title',)}  # Создает слаг из заголовка
+
+    save_as = True  # Добавляет кнопку для быстрого заполения новой новости на основе предыдущей
+    save_on_top = True  # Дублирует кнопки сохранения в верху страницы
+
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo', 'views')  # Отображаемые поля в списке
+    list_display_links = ('id', 'title')  # id и заголовок в виде ссылке в админке в списке
+    list_filter = ('category',)  # Добавляет возможность фильтрации статей по категории
+    search_fields = ('title',)  # Возможность поиска по названию статьи
+
+    readonly_fields = ('views', 'created_at', 'get_photo')  # Поля "только для просмотра"
+    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')  # Отображаемые поля в статье
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50"/>')
+        return "No photo"
+
+    get_photo.short_description = "Фото"  # Описание столбца в админке
 
 
 # Регистрируем в админку
